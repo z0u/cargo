c57c3e90e102a1f4fd86d253e3ae29322f814dca
Alex Fraser <alex@phatcore.com>
2009-07-12 05:50:17 +0000

    - Fixed case of some built-in types. - Improved comments. - Removed iterative GetRange function: will be using ActivateRange instead.   Leaving Mark's name in the list of contributors anyway. - Added Update function: changes to nodes are not applied until the end of   ActivateRange and Pulse. - Improved logic of ActivateRange and Pulse functions to try to minimise   node state changes.

--- Game/Scripts/GameKDTree.py
+++ Game/Scripts/GameKDTree.py
@@ -26,6 +26,7 @@ class StructureError(Exception):
 class Cube:
 	'''A bounding cube with arbitrary dimensions, defined by its centre
 	and radius.'''
+	
 	def __init__(self, centre, radius):
 		self.LowerBound = []
 		self.UpperBound = []
@@ -35,16 +36,17 @@ class Cube:
 	
 	def isInRange(self, point):
 	'''Tests whether a point is inside the cube.
-	Returns: false if the point is outside; true otherwise.'''
+	Returns: False if the point is outside; True otherwise.'''
 		for i in xrange(len(point)):
 			if point[i] < self.LowerBound[i]:
-				return false
+				return False
 			elif point[i] > self.UpperBound[i]:
-				return false
-		return true
+				return False
+		return True
 
 class LODTree:
 	'''A KD-tree of game objects for hierarchical LOD management.'''
+	
 	def __init__(self, root):
 		'''Create a new LODTree from an existing hierarchy of game objects.
 		Each object in the tree should have the following parameters:
@@ -53,42 +55,11 @@ class LODTree:
 		float KDMedianValue'''
 		
 		self.Root = LODNode(root)
-	
-	def getRange(self, centre, radius):
-		'''Get a list of all the leaves in the tree that have objects that are
-		within the given bounds. This is an iterative depth-first search.'''
-		bounds = Cube(centre, radius)
-		leavesInRange = []
-		queue = [self.Root]
-		
-		while queue != []:
-			n = queue.pop()
-			
-			if n.Left: # implies n.Right
-				if n.MedianValue > bounds.LowerBound[n.Axis]:
-					#
-					# Within lower bound on this axis. Check the left tree. We
-					# know this isn't a leaf, so we can assume n.Left exists.
-					#
-					queue.append(n.Left)
-				if n.MedianValue < bounds.UpperBound[n.Axis]:
-					#
-					# Within upper bound on this axis. Check the right tree. We
-					# know this isn't a leaf, so we can assume n.Right exists.
-					#
-					queue.append(n.Right)
-			else:
-				#
-				# Found a leaf node. Still need to test it properly.
-				#
-				for o in n.Owner.children:
-					if bounds.isInRange(o.getWorldPosition()):
-						leavesInRange.append(n)
-						break
-		
-		return leavesInRange
 
 class LODNode:
+	'''A node in an LODTree. This is an abstract class; see LODBranch and
+	LODLeaf.'''
+	
 	def __init__(self, owner):
 		'''Populate this tree with a hierarchy. The hierarchy should have been
 		created previously using the BlenderObjectXYTree.Serialise method.'''
@@ -103,9 +74,9 @@ class LODNode:
 		# Implicit:       This node has no descendants in range, but it is
 		#                 visible because its sibling has been activated.
 		#
-		self.Visible = false
-		self.SubtreeVisible = false
-		self.Implicit = false
+		self.Visible = False
+		self.SubtreeVisible = False
+		self.Implicit = False
 	
 	def ActivateRange(self, bounds):
 		'''
@@ -129,30 +100,43 @@ class LODNode:
 		pass
 	
 	def Show(self):
+		'''Make this node visible. Rendering is deferred: call Update to apply
+		the changes.'''
 		if self.Visible:
 			return
 		
 		assert(not self.SubtreeVisible)
 		
-		self.Owner.visible = true
-		self.Visible = true
-		self.SubtreeVisible = true
+		self.Visible = True
+		self.SubtreeVisible = True
 	
 	def Hide(self):
+		'''Make this node invisible. Rendering is deferred: call Update to apply
+		the changes.'''
 		if not self.Visible:
 			return
-		self.Owner.visible = false
-		self.Visible = false
-		self.SubtreeVisible = false
-		self.Implicit = false
+		
+		self.Visible = False
+		self.SubtreeVisible = False
+		self.Implicit = False
 	
 	def Implicate(self):
 		'''Make this node visible even though no descendant leaves are within
-		range.'''
+		range. This is called when a sibling is visible.'''
 		self.Show()
-		self.Implicit = true
+		self.Implicit = True
+	
+	def Update(self):
+		'''Apply any changes that have been made to this node.'''
+		if self.Owner.visible != self.Visible:
+			self.Owner.visible = self.Visible
 
 class LODBranch(LODNode):
+	'''A branch in an LODTree. This type of node has two children: Left and
+	Right. The left subtree contains all elements whose location on the Axis is
+	less than the MedianValue. The right subtree contains all the other
+	elements.'''
+	
 	def __init__(self, owner):
 		LODNode.__init__(self, owner)
 		
@@ -191,40 +175,62 @@ class LODBranch(LODNode):
 			raise StructureError, "Node is not a leaf, but is missing a branch."
 	
 	def ActivateRange(self, bounds):
+		left = self.Left
+		right = self.Right
+		
 		#
 		# Activate the branches if they are not out of bounds on the current
 		# axis. If they are out of bounds but were previously active, descend in
 		# to them to increment their age.
 		#
-		left = self.Left
-		right = self.Right
-		
 		if self.MedianValue > bounds.LowerBound[n.Axis]:
 			left.ActivateRange(bounds)
 		elif left.SubtreeVisible:
 			left.Pulse()
-			if (not left.SubtreeVisible) and right.Implicit:
-				right.Hide()
 		
 		if self.MedianValue < bounds.UpperBound[n.Axis]:
 			right.ActivateRange(bounds)
 		elif right.SubtreeVisible:
 			right.Pulse()
-			if (not right.SubtreeVisible) and left.Implicit:
-				left.Hide()
 		
 		#
 		# If either branch contains visible nodes, make sure the other branch is
 		# shown too.
 		#
-		if left.SubtreeVisible and not right.SubtreeVisible:
-			right.Implicate()
-		if right.SubtreeVisible and not left.SubtreeVisible:
-			left.Implicate()
+		if left.SubtreeVisible:
+			if not right.SubtreeVisible:
+				right.Implicate()
+		elif right.Implicit:
+			right.Hide()
 		
-		self.SubtreeVisible = self.Visible or left.SubtreeVisible or right.SubtreeVisible
+		if right.SubtreeVisible:
+			if not left.SubtreeVisible:
+				left.Implicate()
+		elif left.Implicit:
+			left.Hide()
+		
+		asser(right.Implicit != left.Implicit)
+		
+		#
+		# No more operations will occur on child nodes for this frame, so render
+		# them now.
+		#
+		left.Update()
+		right.Update()
+		
+		self.SubtreeVisible = left.SubtreeVisible or right.SubtreeVisible
 	
 	def Pulse(self, maxAge):
+		if self.Implicit:
+			#
+			# An implicit branch:
+			# * Is visible.
+			# * Has no visible descendants.
+			# * Depends upon its sibling for visibility (has no age).
+			# Therefore, there is nothing to do here.
+			#
+			return
+		
 		left = self.Left
 		right = self.Right
 		
@@ -238,20 +244,39 @@ class LODBranch(LODNode):
 			if (not right.SubtreeVisible) and left.Implicit:
 				left.Hide()
 		
-		self.SubtreeVisible = self.Visible or left.SubtreeVisible or right.SubtreeVisible
+		#
+		# No more operations will occur on child nodes for this frame, so render
+		# them now.
+		#
+		left.Update()
+		right.Update()
+		
+		self.SubtreeVisible = left.SubtreeVisible or right.SubtreeVisible
+	
+	def Update(self):
+		'''Apply any changes that have been made to this node.'''
+		if self.Owner.visible != self.Visible:
+			self.Owner.visible = self.Visible
 
 class LODLeaf(LODNode):
+	'''A leaf node in an LODTree. A leaf is the bottom of the tree, but it can
+	still have multiple children.'''
+	
 	def __init__(self, owner):
 		LODNode.__init__(self, owner)
 		self.NumFramesActive = -1
 	
 	def ActivateRange(self, bounds, implicitNodes):
+		'''Search the objects owned by this node. If any of them are within
+		range, this node will be shown.'''
 		for o in self.Owner.children:
 			if bounds.isInRange(o.getWorldPosition()):
 				self.Show()
 				return
 	
 	def Pulse(self, maxAge):
+		'''Make this node age by one frame. If it has been visible for too long
+		it will be hidden.'''
 		self.NumFramesActive = self.NumFramesActive + 1
 		if self.NumFramesActive > maxAge:
 			self.Hide()
@@ -263,3 +288,9 @@ class LODLeaf(LODNode):
 	def Hide(self):
 		LODNode.Hide(self)
 		self.NumFramesActive = -1
+	
+	def Update(self):
+		'''Apply any changes that have been made to this node.'''
+		if self.Owner.children[0].visible != self.Visible:
+			for o in self.Owner.children:
+				o.visible = self.Visible

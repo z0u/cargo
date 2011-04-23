b6e4436da3c4856002f8386923666da574243efb
Alex Fraser <alex@phatcore.com>
2011-04-23 15:51:19 +1000

    Ported LOD tree creator over to Blender 2.57 API from 2.49b.

--- Game/BScripts/BlendKDTree.py
+++ Game/BScripts/BlendKDTree.py
@@ -17,7 +17,7 @@
 #
 
 import bpy
-import Blender
+import mathutils
 
 #
 # This is only for debugging: makes the tree print out with indentation, but
@@ -30,7 +30,7 @@ class StateError(Exception):
 	pass
 
 class Progress:
-	def __init__(self, message, upperBound, updateStep = 0.001):
+	def __init__(self, message, upperBound, updateStep = 0.01):
 		self.upper = upperBound
 		self.message = message
 		self.currentValue = 0
@@ -51,19 +51,32 @@ class Progress:
 			self.update()
 
 	def update(self):
-		Blender.Window.DrawProgressBar(self.currentFraction, self.message)
-		self.lastFraction = self.currentFraction
+		'''Show the status to the user.'''
+		pass
+
+class ConsoleProgress(Progress):
+	def update(self):
+		print('%s: %2.0f' % (self.message, self.currentFraction * 100))
+
+class WindowProgress(Progress):
+	def update(self):
+		pass
+		# Needs to be updated for Blender 2.5x
+#		Blender.Window.DrawProgressBar(self.currentFraction, self.message)
+#		self.lastFraction = self.currentFraction
+
+progressFactory = ConsoleProgress
 
 class KDTree:
 	def __init__(self, objects, dimensions, leafSize = 4):
 		self.leafSize = leafSize
 		self.dimensions = dimensions
 		self.maxDepth = 0
-		self.debug = False
 		self._depthIPOs = []
 		self.nNodes = 0
+		self.nObs = len(objects)
 
-		self.progress = Progress('1/3: Constructing KDTree', len(objects))
+		self.progress = progressFactory('1/3: Constructing KDTree', len(objects))
 
 		if len(objects) > leafSize:
 			self.root = KDBranch(objects, 0, self)
@@ -75,29 +88,7 @@ class KDTree:
 
 	def on_leaf_created(self, leaf):
 		self.update_max_depth(leaf.depth)
-		self.progress.increment(len(leaf.Objects))
-
-	def GetIPO(self, level):
-		try:
-			return self._depthIPOs[level]
-		except IndexError:
-			# continued below
-			pass
-
-		for lvl in range(0, self.maxDepth):
-			colour = float(lvl) / float(self.maxDepth)
-			ipo = Blender.Ipo.New('owner', 'LOD_%d' % lvl)
-			ic = ipo.addCurve('ColR')
-			ic[1.0] = colour
-			ic = ipo.addCurve('ColG')
-			ic[1.0] = colour
-			ic = ipo.addCurve('ColB')
-			ic[1.0] = colour
-			ic = ipo.addCurve('ColA')
-			ic[1.0] = 1.0
-			self._depthIPOs.append(ipo)
-
-		return self._depthIPOs[level]
+		self.progress.increment(len(leaf.obs))
 
 	def update_max_depth(self, depth):
 		if depth > self.maxDepth:
@@ -107,7 +98,7 @@ class KDTree:
 		self.progress.increment(1)
 
 	def create_cluster_hierarchy(self):
-		self.progress = Progress('2/3: Creating clusters', self.nNodes)
+		self.progress = progressFactory('2/3: Creating clusters', self.nNodes)
 		self.root.create_cluster_hierarchy([], [])
 
 	def on_node_serialised(self, node):
@@ -132,23 +123,25 @@ class KDTree:
 			...
 
 		Parameters:
-		tBuf: The text buffer to write into (Blender.Text.Text).
+		tBuf: The text buffer to write into (bpy.types.text).
 		'''
-		self.progress = Progress('3/3: Serialising KDTree', self.nNodes)
+		self.progress = progressFactory('3/3: Serialising KDTree', self.nNodes)
 
 		#
 		# Deselect the objects: they will be selected again on serialisation.
 		#
-		Blender.Scene.GetCurrent().objects.selected = []
+		bpy.ops.object.select_all(action='DESELECT')
 
-		tBuf.write('#\n# A serialised LODTree, created by BlendKDTree in the Source/Scripts directory.\n#\n')
-		tBuf.write('import Scripts.LODTree\n')
-		tBuf.write('br = Scripts.LODTree.LODBranch\n')
-		tBuf.write('lf = Scripts.LODTree.LODLeaf\n')
+		tBuf.write('#\n# A serialised LODTree, created by BlendKDTree in the BScripts directory.\n#\n')
+		tBuf.write('# This tree contains %d leaf objects, supported by %d nodes in %d levels\n#\n' %
+				(self.nObs, self.nNodes, self.maxDepth))
+		tBuf.write('import Scripts.lodtree\n')
+		tBuf.write('br = Scripts.lodtree.LODBranch\n')
+		tBuf.write('lf = Scripts.lodtree.LODLeaf\n')
 		tBuf.write('# Queues avoid nested instantiations.\n')
 		tBuf.write('LQ = []\n')
 		tBuf.write('RQ = []\n')
-		self.root.serialise_to_lod_tree(tBuf, '', 'tree=Scripts.LODTree.LODTree(%s)')
+		self.root.serialise_to_lod_tree(tBuf, '', 'tree=Scripts.lodtree.LODTree(%s)')
 		tBuf.write('assert(len(LQ) == 0)\n')
 		tBuf.write('assert(len(RQ) == 0)\n')
 
@@ -167,15 +160,17 @@ class KDBranch(KDNode):
 		#
 		# Sort the objects along the current axis.
 		#
-		objects.sort(lambda a, b: cmp(a.getLocation('localspace')[self.axis],
-		                              b.getLocation('localspace')[self.axis]))
+		def dk(ob):
+			return ob.location[self.axis]
+
+		os = objects.sort(key=dk)
 
 		#
 		# The median value is the location of the middle element on the current
 		# axis.
 		#
-		medianIndex = len(objects) / 2
-		self.medianValue = objects[medianIndex].getLocation('worldspace')[self.axis]
+		medianIndex = int(len(objects) / 2)
+		self.medianValue = objects[medianIndex].location[self.axis]
 
 		#
 		# Create children.
@@ -217,44 +212,47 @@ class KDBranch(KDNode):
 		# Create a new, empty object.
 		#
 		name = 'LOD_%d%s' % (self.depth, side)
-		sce = Blender.Scene.GetCurrent()
-		mesh = bpy.data.meshes.new(name)
-		ob = sce.objects.new(mesh, name)
+		bpy.ops.object.add(type='MESH')
+		ob = bpy.context.object
+		ob.name = name
+		ob.data.name = name
 
 		#
-		# Set the location. For setLocation, localspace == worldspace: the new
+		# Set the location. For location, localspace == worldspace: the new
 		# object has no parent yet.
 		#
-		meanLoc = Blender.Mathutils.Vector()
+		meanLoc = mathutils.Vector((0,0,0))
 		for c in childPosObs:
-			loc = Blender.Mathutils.Vector(c.getLocation('worldspace'))
-			meanLoc = meanLoc + loc
+			meanLoc = meanLoc + c.location
 		meanLoc = meanLoc / len(childPosObs)
-		ob.setLocation(meanLoc)
+		ob.location = meanLoc
 
 		#
-		# Create a new cluster from children.
+		# Create a new cluster from children. First, make linked duplicates of
+		# the children so the old objects are retained.
 		#
-		ob.join(childMeshObs)
-
-		if self.tree.debug:
-			#
-			# Colour the objects for debugging.
-			#
-			ob.setIpo(self.tree.GetIPO(self.depth))
+		bpy.ops.object.select_all(action='DESELECT')
+		for child in childMeshObs:
+			child.select = True
+		bpy.ops.object.duplicate(linked=True)
+		ob.select = True
+		bpy.context.scene.objects.active = ob
+		bpy.ops.object.join()
 
 		#
 		# Parent children to new cluster. This relationship will be broken by
 		# LODTree on deserialisation (when the game starts). In the mean time,
 		# the relationships make the objects easier to manage.
 		#
-		ob.makeParent(childPosObs)
+		for child in childPosObs:
+			child.select = True
+		bpy.ops.object.parent_set()
 
 		#
 		# Disable fancy collisions. Only the leaves retain their original
 		# interactivity.
 		#
-		ob.rbFlags = Blender.Object.RBFlags['PROP']
+		ob.game.physics_type = 'NO_COLLISION'
 
 		self.owner = ob
 		self.tree.on_cluster_created(self)
@@ -274,7 +272,7 @@ class KDBranch(KDNode):
 		according to the 'format' parameter (see below).
 
 		Parameters:
-		tBuf:   The text buffer to write into (Blender.Text.Text).
+		tBuf:   The text buffer to write into (bpy.types.text).
 		indent: The spacing to insert at the start of each line (string).
 		format: Extra string formatting for the serialised object. This is used
 		        to assign the new object to a variable. (string)
@@ -285,17 +283,16 @@ class KDBranch(KDNode):
 		self.left.serialise_to_lod_tree(tBuf, indent + INDENT_STEP, 'LQ.append(%s)')
 		self.right.serialise_to_lod_tree(tBuf, indent + INDENT_STEP, 'RQ.append(%s)')
 
-		obName = self.owner.getName()
+		obName = self.owner.name
 		expr = 'br(\'%s\',LQ.pop(),RQ.pop(),%d,%f)' % (obName, self.axis, self.medianValue)
 		tBuf.write(indent + (format % expr) + (' # %d' % self.depth)  + '\n')
-		self.owner.select(True)
 		self.tree.on_node_serialised(self)
 
 class KDLeaf(KDNode):
 	def __init__(self, objects, depth, tree):
 		KDNode.__init__(self, depth, tree)
-		self.Objects = objects
-		self.SerialisableObjects = []
+		self.obs = objects
+		self.serialisableObs = []
 		tree.on_leaf_created(self)
 
 	def create_cluster_hierarchy(self, meshObs, posObs, side = ''):
@@ -309,34 +306,39 @@ class KDLeaf(KDNode):
 		         to this list. In the case of KDLeaf, the objects will be
 		         empties. Each empty will have a property that 
 		'''
-		if len(self.SerialisableObjects) > 0:
+		if len(self.serialisableObs) > 0:
 			raise StateError('Tried to create cluster twice.')
 
-		for o in self.Objects:
-			ps = o.getAllProperties()
-			prop = None
-			for p in ps:
-				if p.name == 'LODObject':
-					prop = p
-					break
-			if prop:
+		for o in self.obs:
+			if 'LODObject' in o.game.properties:
 				#
 				# If the object has an 'LODObject' property, create a new Empty
 				# to stand in its place.
 				#
-				e = Blender.Scene.GetCurrent().objects.new('Empty', 'E' + prop.data)
-				#e = Blender.Object.New('Empty', 'E' + o.getName())
-				e.setLocation(o.getLocation('worldspace'))
-				e.setEuler(o.getEuler('worldspace'))
-				e.addProperty(prop.name, prop.data, prop.type)
+				bpy.ops.object.add(type='EMPTY')
+				e = bpy.context.object
+				e.name = 'E' + o.game.properties['LODObject'].value
+
+				e.matrix_world = o.matrix_world
+
+				#
+				# Copy the name of the LOD object into a property on the new
+				# empty object.
+				# 
+				bpy.ops.object.select_all(action='DESELECT')
+				o.select = True
+				e.select = True
+				bpy.context.scene.objects.active = o
+				bpy.ops.object.game_property_copy(property='LODObject')
+
 				meshObs.append(o)
 				posObs.append(e)
-				self.SerialisableObjects.append(e)
+				self.serialisableObs.append(e)
 
 			else:
 				meshObs.append(o)
 				posObs.append(o)
-				self.SerialisableObjects.append(o)
+				self.serialisableObs.append(o)
 
 		self.tree.on_cluster_created(self)
 
@@ -352,26 +354,26 @@ class KDLeaf(KDNode):
 		to the 'format' parameter (see below).
 
 		Parameters:
-		tBuf:   The text buffer to write into (Blender.Text.Text).
+		tBuf:   The text buffer to write into (bpy.types.text).
 		indent: The spacing to insert at the start of each line (string).
 		format: Extra string formatting for the serialised object. This is used
 		        to assign the new object to a variable. (string)
 		'''
-		if len(self.SerialisableObjects) < len(self.Objects):
+		if len(self.serialisableObs) < len(self.obs):
 			raise StateError('Serialisation requires clusters to have been created.')
 
 		elements = []
-		for e in self.SerialisableObjects:
-			elements.append(e.getName())
-			e.select(True)
+		for e in self.serialisableObs:
+			elements.append(e.name)
+			e.select = True
 		expr = 'lf(%s)' % repr(elements)
 		tBuf.write(indent + (format % expr) + (' # %d' % self.depth)  + '\n')
 		self.tree.on_node_serialised(self)
 
 if __name__ == '__main__':
 	'''Create clusters and a serialised LOD tree from the selected objects.'''
-	obs = Blender.Object.GetSelected()
+	obs = bpy.context.selected_objects
 	tree = KDTree(obs, dimensions = 2, leafSize = 4)
-	tBuf = Blender.Text.New('LODTree_Serialised')
+	tBuf = bpy.data.texts.new(name='LODTree_Serialised')
 	tree.serialise_to_lod_tree(tBuf)
-	Blender.Draw.PupMenu('Tree created.%t|' + ('Saved to text buffer %s.' % tBuf.getName()))
+	print('Tree created.%t|' + ('Saved to text buffer %s.' % tBuf.name))

cf3dd8fdfe47a5e39a377546106e505cc2e2081a
Alex Fraser <alex@phatcore.com>
2011-06-13 20:42:19 +1000

    DoF shader updated to match release to BlenderArtists and on my blog. Uses slighly better sampling. Shader now has a 2-clause (simplified) BSD licence.

--- Game/Shaders/dof.glsl
+++ Game/Shaders/dof.glsl
@@ -1,18 +1,30 @@
 //
-// Copyright 2011 Alex Fraser <alex@phatcore.com>
+// Copyright 2011 Alex Fraser. All rights reserved.
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+//   1. Redistributions of source code must retain the above copyright notice,
+//      this list of conditions and the following disclaimer.
 //
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//   2. Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY ALEX FRASER ``AS IS'' AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+// EVENT SHALL ALEX FRASER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+// OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+// EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// The views and conclusions contained in the software and documentation are
+// those of the authors and should not be interpreted as representing official
+// policies, either expressed or implied, of Alex Fraser.
 //
 
 // Fragment shader
@@ -26,7 +38,13 @@ uniform float bgl_RenderedTextureWidth;
 uniform float bgl_RenderedTextureHeight;
 
 //
-// Uniforms set as game object properties (see logic buttons)
+// Uniforms set as game object properties (see logic buttons). focalDepth should
+// be set as:
+//
+//     1.0 - zNear / z
+//
+// Where zNear is the distance to the near clip plane, and the z is the distance
+// to the focal plane, both in world units.
 //
 uniform float focalDepth;
 uniform float blurRadius;
@@ -91,13 +109,13 @@ vec4 blur_sample(float depth, vec2 blur, vec2 offset, inout float influence) {
 }
 
 void main(void) {
-    vec4 col = get_colour(vec2(0.0));
     float depth = get_depth(vec2(0.0));
     float aspect = bgl_RenderedTextureWidth / bgl_RenderedTextureHeight;
     vec2 blur = vec2(get_blur(depth)) * vec2(1.0, aspect);
-    float influence = 1.0;
+    vec4 col = vec4(0.0);
+    float influence = 0.000001;
 
-    // Bokeh: Spiral
+    // Bokeh: Spiral (33 samples)
     col += blur_sample(depth, blur, vec2(0.727598, -0.083846), influence);
     col += blur_sample(depth, blur, vec2(-0.232845, 0.369841), influence);
     col += blur_sample(depth, blur, vec2(-0.361229, 0.069334), influence);

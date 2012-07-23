8d6713bc8715e80748722287f78ffadbd52e9ae5
Alex Fraser <alex@phatcore.com>
2012-07-23 18:28:07 +1000

    Fixed partial letterboxing issue when loading a new file.

--- Game/Scripts/ui.py
+++ Game/Scripts/ui.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2009-2010 Alex Fraser <alex@phatcore.com>
+# Copyright 2009-2012 Alex Fraser <alex@phatcore.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,7 +15,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-import weakref
 import time
 
 import bge

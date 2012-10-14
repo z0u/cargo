025e83891b55325280670100df2aa5ffe2eb22ec
Alex Fraser <alex@phatcore.com>
2012-10-14 12:29:35 +1100

    Improved ant tree enter animation

--- Game/Scripts/story_ant.py
+++ Game/Scripts/story_ant.py
@@ -230,36 +230,41 @@ class Ant(bat.story.Chapter, bat.bats.BX_GameObject, bge.types.BL_ArmatureObject
 		s.add_action(Scripts.story.ActSetCamera('AntVictoryCam'))
 		s.add_event("ShowDialogue", "Amazing! You've done it!")
 		s.add_event("TeleportSnail", "HP_SnailTalkPos")
+		sbreathe = s.create_sub_step()
+		sbreathe.add_condition(bat.story.CondActionGE(Ant.L_ANIM, 40, tap=True))
+		sbreathe.add_action(bat.story.ActAction('HP_AntEnter', 40, 80, Ant.L_ANIM,
+				play_mode=bge.logic.KX_ACTION_MODE_LOOP))
 
 		s = s.create_successor()
 		s.add_condition(bat.story.CondActionGE(Ant.L_ANIM, 40))
 		s.add_condition(bat.story.CondEvent("DialogueDismissed", self))
 		s.add_action(Scripts.story.ActSetCamera('AntSniffCam'))
 		s.add_event("ShowDialogue", "And look, it's just as I suspected - sugary syrup!")
-		s.add_action(bat.story.ActAction('HP_AntEnter', 50, 70, Ant.L_ANIM))
+		s.add_action(bat.story.ActAction('HP_AntEnter', 80, 100, Ant.L_ANIM,
+				blendin=3.0))
 		ssniff = s.create_sub_step("Sniff")
-		ssniff.add_condition(bat.story.CondActionGE(Ant.L_ANIM, 70, tap=True))
-		ssniff.add_action(bat.story.ActAction('HP_AntEnter', 70, 102, Ant.L_ANIM,
+		ssniff.add_condition(bat.story.CondActionGE(Ant.L_ANIM, 100, tap=True))
+		ssniff.add_action(bat.story.ActAction('HP_AntEnter', 100, 132, Ant.L_ANIM,
 				play_mode=bge.logic.KX_ACTION_MODE_LOOP))
 
 		s = s.create_successor()
-		s.add_condition(bat.story.CondActionGE(Ant.L_ANIM, 70))
+		s.add_condition(bat.story.CondActionGE(Ant.L_ANIM, 100))
 		s.add_condition(bat.story.CondEvent("DialogueDismissed", self))
 		s.add_event("ShowDialogue", "Let's find the source. Last one up the tree is a rotten egg!")
-		s.add_action(bat.story.ActAction('HP_AntEnter', 102, 123, Ant.L_ANIM,
+		s.add_action(bat.story.ActAction('HP_AntEnter', 132, 153, Ant.L_ANIM,
 				blendin=3.0))
 		slook = s.create_sub_step("look")
-		slook.add_condition(bat.story.CondActionGE(Ant.L_ANIM, 123, tap=True))
-		slook.add_action(bat.story.ActAction('HP_AntEnter', 123, 158, Ant.L_ANIM,
+		slook.add_condition(bat.story.CondActionGE(Ant.L_ANIM, 153, tap=True))
+		slook.add_action(bat.story.ActAction('HP_AntEnter', 153, 188, Ant.L_ANIM,
 				play_mode=bge.logic.KX_ACTION_MODE_LOOP))
 
 		s = s.create_successor()
 		s.add_condition(bat.story.CondEvent("DialogueDismissed", self))
-		s.add_action(bat.story.ActAction('HP_AntEnter', 158, 190, Ant.L_ANIM,
+		s.add_action(bat.story.ActAction('HP_AntEnter', 188, 230, Ant.L_ANIM,
 				blendin=3.0))
 
 		senter_end = s.create_successor()
-		senter_end.add_condition(bat.story.CondActionGE(Ant.L_ANIM, 189))
+		senter_end.add_condition(bat.story.CondActionGE(Ant.L_ANIM, 230))
 		senter_end.add_action(Scripts.story.ActRemoveCamera('AntSniffCam'))
 		senter_end.add_action(Scripts.story.ActRemoveCamera('AntVictoryCam'))
 		senter_end.add_action(Scripts.story.ActRemoveCamera('AntMidCam'))

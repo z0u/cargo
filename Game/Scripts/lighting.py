3ae1579e425357d6a0a0e7285ac179b95573c0c5
Alex Fraser <alex@phatcore.com>
2012-12-15 22:01:22 +1100

    Lights now fade in and out when moving between rooms. Lights that are not changing nodes do not fade at all, so the player shouldn't even notice the lights moving.

--- Game/Scripts/lighting.py
+++ Game/Scripts/lighting.py
@@ -28,11 +28,83 @@ import bat.bats
 import Scripts.director
 
 class LightNode:
+	lamp = bat.containers.weakprop('lamp')
+
 	def __init__(self, index, pos, colour):
 		self.index = index
 		self.neighbours = set()
 		self.pos = pos
 		self.colour = colour
+
+	def __str__(self):
+		return "LightNode(%d)" % self.index
+
+
+class Lamp:
+	log = logging.getLogger(__name__ + '.Lamp')
+
+	FADE_RATE = 0.1
+
+	def __init__(self, ob):
+		self.ob = ob
+		self.default_energy = ob.energy
+		self.current_node = None
+		self.target_node = None
+
+	@property
+	def invalid(self):
+		return self.ob is not None and not self.ob.invalid
+
+	def update(self):
+		ob = self.ob
+		if self.current_node is not self.target_node:
+			if ob.energy <= 0.0:
+				# Lamp is currently off; relocation is OK.
+				self.relocate()
+			else:
+				self.fade_out()
+		elif self.target_node is None:
+			self.fade_out()
+		else:
+			self.fade_in()
+
+	def fade_in(self):
+		ob = self.ob
+		if ob.energy >= self.default_energy:
+			return
+
+		ob.energy += Lamp.FADE_RATE
+		if ob.energy >= self.default_energy:
+			# Transfer finished.
+			Lamp.log.debug("%s fade in complete at %s.", ob.name, self.current_node)
+			ob.energy = self.default_energy
+
+	def fade_out(self):
+		ob = self.ob
+		if ob.energy <= 0.0:
+			return
+
+		ob.energy -= Lamp.FADE_RATE
+		if ob.energy <= 0.0:
+			# Transfer.
+			Lamp.log.debug("%s fade out complete at %s.", self, self.current_node)
+			ob.energy = 0.0
+			self.relocate()
+
+	def relocate(self):
+		ob = self.ob
+		self.current_node = self.target_node
+		if self.target_node is not None:
+			Lamp.log.debug("%s fading in at %s.", self, self.current_node)
+			ob.worldPosition = self.target_node.pos
+			if self.target_node.colour is not None:
+				ob.color = self.target_node.colour
+		else:
+			ob.energy = 0.0
+
+	def __str__(self):
+		return "Lamp(%s)" % self.ob.name
+
 
 class LightNetwork(bat.bats.BX_GameObject, bge.types.KX_GameObject):
 	'''
@@ -71,12 +143,10 @@ class LightNetwork(bat.bats.BX_GameObject, bge.types.KX_GameObject):
 		sce = self.scene
 		for i in range(LightNetwork.MAX_LAMPS):
 			try:
-				lamp = sce.objects['UserLight%d' % (i + 1)]
-				lamp['_default_colour'] = lamp.color
-				lamp['_default_energy'] = lamp.energy
-				self.lamps.append(lamp)
+				lamp_ob = sce.objects['UserLight%d' % (i + 1)]
 			except KeyError:
-				pass
+				continue
+			self.lamps.append(Lamp(lamp_ob))
 		LightNetwork.log.info("Found %d lamps", len(self.lamps))
 		if len(self.lamps) < 3:
 			LightNetwork.log.info("Found few lamps. Lamp objects should be named UserLightN.")
@@ -104,7 +174,10 @@ class LightNetwork(bat.bats.BX_GameObject, bge.types.KX_GameObject):
 						break
 				if not dup:
 					positions.append(pos)
-					colours.append(vert.color.copy())
+					if self.use_colours:
+						colours.append(vert.color.xyz)
+					else:
+						colours.append(None)
 
 			if len(positions) != 2:
 				raise ValueError("Edge has %d vertices: %s" % (len(positions), positions))
@@ -133,41 +206,71 @@ class LightNetwork(bat.bats.BX_GameObject, bge.types.KX_GameObject):
 			return
 		pos = player.worldPosition.copy()
 		node = self.find_closest_node(pos)
-		if node is None or node is self.current_node:
-			return
+		if node is not None and node is not self.current_node:
+			self.activate_node(node)
 
-		self.activate_node(node)
+		for lamp in self.lamps:
+			lamp.update()
 
 	def find_closest_node(self, pos):
 		def distance_key(_n):
 			return (pos - _n.pos).magnitude
 		self.nodes.sort(key=distance_key)
+
+		# Just test closest node. Testing further nodes would result in an
+		# unstable state: the lights flicker when the node becomes obscured
+		# momentarily.
 		n = self.nodes[0]
 
 		dist = (n.pos - pos).magnitude
 		hit_ob, _, _ = self.rayCast(n.pos, pos, dist, 'Ray')
 		if hit_ob is None or hit_ob is self:
-			LightNetwork.log.debug('Hit node %d', n.index)
 			return n
 		else:
-			LightNetwork.log.debug('Missed node %d, dist: %f, hit %s', n.index, dist, hit_ob.name)
 			return None
 
 	def activate_node(self, node):
 		LightNetwork.log.info('Activating node %d', node.index)
 		self.current_node = node
-		nodes = [node]
-		nodes.extend(node.neighbours)
-		for n, lamp in zip(nodes, self.lamps):
-			lamp.worldPosition = n.pos
-			lamp.energy = lamp['_default_energy']
-			if self.use_colours:
-				lamp.color = node.colour
 
-		LightNetwork.log.info('Using %d lamps', len(nodes))
-		if len(self.lamps) < len(nodes):
-			LightNetwork.log.warn('Not enough user lights to satisfy node. %d lights required.', len(nodes))
+		# Position a light at the current node, and all of its neighbours.
+		nodes = set()
+		nodes.add(node)
+		nodes.update(node.neighbours)
+
+		# Find out which lamps need to move. Don't just reassign them, or they
+		# will all turn off and then back on.
+		occupied_nodes = set()
+		available_lamps = []
+		for lamp in self.lamps:
+			if lamp.target_node is None:
+				# Prefer to assign unused lamps.
+				LightNetwork.log.debug('%s is unused.', lamp)
+				available_lamps.insert(0, lamp)
+			elif lamp.target_node in nodes:
+				# Can't assign this lamp.
+				LightNetwork.log.debug('%s can not be moved.', lamp)
+				occupied_nodes.add(lamp.target_node)
+			else:
+				# Reassign lamp that is already in use.
+				LightNetwork.log.debug('%s can be moved.', lamp)
+				available_lamps.append(lamp)
+
+		unoccupied_nodes = nodes.difference(occupied_nodes)
+		LightNetwork.log.debug('Using %d, reusing %d, moving %d', len(nodes),
+				len(occupied_nodes), len(unoccupied_nodes))
+
+		# Assign lamps, starting with the ones that are not currently being
+		# used.
+		for n in unoccupied_nodes.copy():
+			try:
+				lamp = available_lamps.pop(0)
+			except IndexError:
+				LightNetwork.log.warn('Not enough user lights to satisfy node. %d lights required.', len(nodes))
+				break
+			lamp.target_node = n
 
 		# Deactivate remaining lamps.
-		for lamp in self.lamps[len(nodes):]:
-			lamp.energy = 0.0
+		for lamp in available_lamps:
+			
+			lamp.target_node = None

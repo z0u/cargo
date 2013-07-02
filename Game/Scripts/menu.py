a0722aa5685c0a14f54cf5cc38fc040c9876c98c
Alex Fraser <alex@phatcore.com>
2013-07-02 22:40:40 +1000

    Added Leigh to credits file

--- Game/Scripts/menu.py
+++ Game/Scripts/menu.py
@@ -36,7 +36,7 @@ CREDITS = [
 	("Textures", "Alex Fraser, Junki Wano"),
 	("Music", "Robert Leigh"),
 	("Programming", "Alex Fraser, Mark Triggs, Campbell Barton, Ben Sturmfels"),
-	("Sound Effects", "Alex Fraser, Ben Sturmfels, Ben Finney, freesound.org users: "
+	("Sound Effects", "Alex Fraser, Ben Sturmfels, Ben Finney, Leigh Raymond, freesound.org users: "
 			"3bagbrew, FreqMan, HerbertBoland, Percy Duke, klakmart, aUREa, "
 			"qubodup, thetruwu, nsp, kangaroovindaloo, ERH, Corsica_S, "
 			"batchku, satrebor, gherat, ZeSoundResearchInc., CGEffex, "

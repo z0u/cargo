008d4a0dfe2a4944c3d20530d93d0025f8293ccb
Alex Fraser <alex@phatcore.com>
2013-09-15 20:58:32 +1000

    Added frames around epilogue drawings.

--- game/assets/Scripts/credits.py
+++ game/assets/Scripts/credits.py
@@ -36,14 +36,14 @@ ANNOTATIONS = [
     ]
 
 IMAGES = [
+    'epilogue_saucebar',
+    'epilogue_firefly',
     'concept_snail',
     'concept_story',
     'concept_lod',
     'concept_tree',
     'concept_worm',
     'concept_catapult',
-    'epilogue_saucebar',
-    'epilogue_firefly',
     'epilogue_saucebar',
     'epilogue_firefly',
     'logo_smidgin',

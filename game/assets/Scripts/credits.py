2febf5dad8ef884dac8d3eb3e64e5b98542591d9
Alex Fraser <alex@phatcore.com>
2013-09-17 22:44:15 +1000

    Updated credits sequence: put all the epilogue images up front, and improved the ordering of other credits items. Credits timing is good now: company logo is centred when music stops.

--- game/assets/Scripts/credits.py
+++ game/assets/Scripts/credits.py
@@ -11,46 +11,71 @@ import bat.containers
 import bat.sound
 
 
-CREDITS = [
+_CREDIT_ITEMS = [
+    'epilogue_saucebar',
+    'epilogue_firefly',
+    'epilogue_ant',
+    'epilogue_saucebar',
+
+    'None',
+
     ("A Game By", ["Alex Fraser"]),
+
+    ("With excellent contributions from...", []),
     ("Story", ["Lara Micocki", "Lev Lafayette", "Jodie Fraser"]),
+
     ("Concept Art", ["Junki Wano"]),
+    'concept_house',
+
     ("Modelling", ["Junki Wano", "Blender Foundation"]),
+    'concept_truck',
+
 #     ("Animation", []),
     ("Textures", ["Junki Wano"]),
+
     ("Music", ["Robert Leigh"]),
+    'concept_worm',
+
     ("Programming", ["Mark Triggs", "Campbell Barton", "Ben Sturmfels",
             "Ashima Arts"]),
+    'concept_lod',
+
     ("Sound Effects", ["Ben Sturmfels", "Ben Finney", "Leigh Raymond"]),
     ("And freesound.org users",
             ["3bagbrew", "FreqMan", "HerbertBoland", "Percy Duke", "klakmart", "aUREa",
             "qubodup", "thetruwu", "nsp", "kangaroovindaloo", "ERH", "Corsica_S",
             "batchku", "satrebor", "gherat", "ZeSoundResearchInc.", "CGEffex",
             "UncleSigmund", "dobroide"]),
+    'concept_tree',
+
     ("Testing", ["Jodie Fraser", "Lachlan Kanaley", "Damien Elmes", "Mark Triggs", "Caley Finn"]),
     ("Made With", ["Blender", "Bullet", "The GIMP", "Inkscape", "Audacity", "Eclipse", "Git"]),
-    ]
-
-ANNOTATIONS = [
-    "With excellent contributions from...",
-    ]
-
-IMAGES = [
-    'epilogue_saucebar',
-    'epilogue_firefly',
-    'epilogue_ant',
-    'epilogue_saucebar',
-    'concept_snail',
-    'concept_story',
-    'concept_lod',
-    'concept_tree',
-    'concept_worm',
     'concept_catapult',
+
+    'None',
+
     'logo_smidgin',
     ]
 
-TRANSLATION_STEP = 0.005
+CREDITS = [item for item in _CREDIT_ITEMS
+        if isinstance(item, tuple) and len(item[1]) > 0]
+
+TRANSLATION_STEP = 0.004
 # TRANSLATION_STEP = 0.02
+
+def plate_generator(items):
+    sce = bge.logic.getCurrentScene()
+    for item in items:
+        if isinstance(item, str):
+            # Image (opaque/dumb object)
+            yield bat.bats.add_and_mutate_object(sce, item)
+        else:
+            # Text object
+            title, people = item
+            plate_ob = bat.bats.add_and_mutate_object(sce, 'NamePlate')
+            plate_ob.title = title
+            plate_ob.people = '\n'.join(people)
+            yield plate_ob
 
 def flatzip(*iterables):
     iters = [iter(iterable) for iterable in iterables]
@@ -61,28 +86,6 @@ def flatzip(*iterables):
                 yield(next(i))
             except StopIteration:
                 iters.remove(i)
-
-def name_plate_generator():
-    sce = bge.logic.getCurrentScene()
-    for title, names in CREDITS:
-        plate_ob = bat.bats.add_and_mutate_object(sce, 'NamePlate')
-        plate_ob.title = title
-        plate_ob.people = '\n'.join(names)
-        yield plate_ob
-
-def annotation_generator():
-    sce = bge.logic.getCurrentScene()
-    for annotation in ANNOTATIONS:
-        plate_ob = bat.bats.add_and_mutate_object(sce, 'NamePlate')
-        plate_ob.title = annotation
-        plate_ob.people = ''
-        yield plate_ob
-
-def image_generator():
-    sce = bge.logic.getCurrentScene()
-    for image in IMAGES:
-        plate_ob = bat.bats.add_and_mutate_object(sce, image)
-        yield plate_ob
 
 current_plates = bat.containers.SafeList()
 def update(c):
@@ -97,12 +100,12 @@ def update(c):
         if current_plates[0].basepos > sce.objects['PlateKill'].worldPosition.y:
             current_plates[0].endObject()
 
-upcoming_plates = flatzip(name_plate_generator(), image_generator(), annotation_generator())
+plates = plate_generator(_CREDIT_ITEMS)
 def spawn_next_plate():
     sce = bge.logic.getCurrentScene()
     spawn = sce.objects['PlateSpawn']
     try:
-        plate = next(upcoming_plates)
+        plate = next(plates)
         plate.worldPosition = spawn.worldPosition
     except StopIteration:
         return

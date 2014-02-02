c6c98c0eb06f6769f39e182cce2a0586ae8fc60e
Alex Fraser <alex@phatcore.com>
2014-02-02 23:10:52 +1100

    Blend2Ascii: Added GPL licence block.

--- game/assets/BScripts/ascblend.py
+++ game/assets/BScripts/ascblend.py
@@ -1,3 +1,31 @@
+#
+# Copyright 2014 Alex Fraser <alex@phatcore.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+'''
+Saves Blender data as text. You can run this as an export addon, or
+from the command line like this:
+
+    blender -b input.blend -P ascblend.py -- output.txt
+
+For help:
+
+    blender -b -P ascblend.py -- -h
+'''
+
 import itertools
 import time
 
